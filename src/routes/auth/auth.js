8e1336fc32c3c1626f6d23d207122b47eb1bb7fa
Chrahuldeveloper <chrahulofficial@gmail.com>
2026-02-17 04:02:52 +0530

    dn

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -1,5 +1,6 @@
 import express from 'express'
 const app = express()
+import bcrypt from "bcrypt";
 import { pool } from '../../../dbconfig'
 
 app.get("/api/v1/auth/register", async (req, res) => {
@@ -8,7 +9,6 @@ app.get("/api/v1/auth/register", async (req, res) => {
         const client = await pool.connect();
 
         const { name, email, password, role } = req.body;
-
 
         const userExists = await client.query("SELECT user_id FROM Users WHERE email=$1", [email])
 
@@ -20,7 +20,9 @@ app.get("/api/v1/auth/register", async (req, res) => {
 
         }
 
-        const res = await client.query("INSERT INTO Users (name, email, password , role) VALUES ($1, $2, $3,$4)",[name, email, password, role])
+        const passwordHash = await bcrypt.hash(password, 12);
+
+        await client.query("INSERT INTO Users (name, email, password , role) VALUES ($1, $2, $3,$4)", [name, email, passwordHash, role])
 
         res.status(201).json({
             message: "User registered successfully",
@@ -35,4 +37,54 @@ app.get("/api/v1/auth/register", async (req, res) => {
 })
 
 
-app.get("/api/v1/auth/login")
+app.get("/api/v1/auth/login", async (req, res) => {
+    try {
+
+        const { email, password } = req.body
+
+        const result = await pool.query("SELECT user_id FROM Users WHERE email=$!", [email])
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+
+        const user = result.rows[0];
+
+        const isValid = await bcrypt.compare(
+            password,
+            user.password_hash
+        );
+
+        if (!isValid) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user.id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+
+    } catch (error) {
+        console.log(error)
+    }
+})
